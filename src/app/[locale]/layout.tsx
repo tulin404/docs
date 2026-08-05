561a63f42862a325dc9d456e4984e805f25cb100
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-05 16:57:32 -0300

    Metadata structured

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,10 +7,31 @@ export async function generateMetadata({
     params: Params
 }): Promise<Metadata> {
     const { locale, slug } = await params;
+
+    switch(locale) {
+        case "pt": 
+            return {
+                title: "Documentação de tulin404",
+                description: "Documentação oficial para orientar, referências de APIs, tutoriais e exemplos."
+            };
+        case "en": 
+            return {
+                title: "tulin404's docs",
+                description: "Official documentation for guides, API references, tutorials, and examples."
+            };
+        case "es": 
+            return {
+                title: "Documentación de tulin404",
+                description: "Documentación oficial de orientación, referencias de API, tutoriales y ejemplos."
+            }
+    }
 }
 
-export default function Home() {
+export default async function Layout() {
     return (
-        <html></html>
+        <html
+            lang={locale}
+        >
+        </html>
     )
 };
