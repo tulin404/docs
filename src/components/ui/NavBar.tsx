3d7f042ae84d864cd34db6f2cd36d5286ead6674
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-05 21:31:42 -0300

    Started navBar

---
+++ src/components/ui/NavBar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function NavBar({
+    content
+} : {
+    content: any // eslint-disable-line
+}) {
+    const pathname = usePathname();
+    
+    return (
+        <Link
+            
+        />
+    );
+};
