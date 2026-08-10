06a1bc1bea54caf1b52b7dcff12305342eecc81f
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-10 17:15:41 -0300

    Flag added

--- src/components/layout/NavBar/components/ChangeLang.tsx
+++ src/components/layout/NavBar/components/ChangeLang.tsx
@@ -3,6 +3,8 @@
 import type { Locale } from "@/types/props";
 import { useState } from "react";
 import { ChevronDown } from "lucide-react";
+import ReactCountryFlag from "react-country-flag";
+import { localeToCountry } from "@/lib/utils/formatters";
 
 export function ChangeLang({
     locale
@@ -13,8 +15,20 @@ export function ChangeLang({
     const [currentLocale, setCurrentLocale] = useState<Locale>(locale);
     
     return (
-        <button onClick={() => setIsSelecting(isSelecting => !isSelecting)} className="flex items-center gap-3 border-border border-2 text-text px-3 py-1 rounded-md">
-            {currentLocale.toUpperCase()}
+        <button onClick={() => setIsSelecting(isSelecting => !isSelecting)} className="flex items-center gap-3 border-border border-2 text-text px-3 py-1 rounded-md hover:border-border-hover transition-colors duration-200">
+            <div className="flex items-center gap-2">
+                <ReactCountryFlag
+                    countryCode={localeToCountry(currentLocale)}
+                    svg
+                    style={{
+                        width: "18px",
+                        height: "18px",
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                    }}
+                />
+                <span>{currentLocale.toUpperCase()}</span>
+            </div>
             <ChevronDown className={`
                     ${isSelecting
                         ?
