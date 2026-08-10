ea2f109da152b11110eb398d2a19412bdf5c41e2
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-10 16:50:59 -0300

    NavBar left side started

--- src/components/layout/NavBar/NavBar.tsx
+++ src/components/layout/NavBar/NavBar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { Locale } from "@/types/props";
+import type { Locale } from "@/types/props";
 import Link from "next/link";
 import { Logo } from "@/components/layout/Logo";
 import { useState, useEffect } from "react";
+import { GitHub } from "@/components/ui/GitHub";
+import { ChangeLang } from "./components/ChangeLang";
 
 export function NavBar({
     locale,
@@ -33,14 +35,17 @@ export function NavBar({
                     :
                     "border-transparent"
                 }
-                fixed bg-background border-b-2 px-12 py-4 transition-colors duration-200
+                fixed flex justify-between w-full bg-background border-b-2 px-12 py-4 transition-colors duration-200
             `}>
             {/* NAV LEFT */}
             <div className="flex items-center gap-10">
                 <Logo locale={locale} />
                 <Link
                     href={`/${locale}/docs`}
-                    className="text-text font-primary font-medium"
+                    className={`
+                        relative text-text font-primary font-medium
+                        after:absolute after:-inset-2 after:-z-10 after:rounded-full after:bg-text/20 after:blur-lg after:opacity-0 hover:after:opacity-100 after:transition-opacity after:duration-400 after:delay-100
+                        `}
                 >
                     Docs
                 </Link>
@@ -48,7 +53,8 @@ export function NavBar({
 
             {/* NAV RIGHT */}
             <div className="flex items-center">
-                
+                <ChangeLang locale={locale} />
+                <GitHub />
             </div>
         </nav>
     );
