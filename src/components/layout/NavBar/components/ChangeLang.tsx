f4b507160c60e88ab58f62f6d80244cc02e3d00b
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-10 20:18:04 -0300

    Dropdown animated

--- src/components/layout/NavBar/components/ChangeLang.tsx
+++ src/components/layout/NavBar/components/ChangeLang.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { ChevronDown } from "lucide-react";
 import ReactCountryFlag from "react-country-flag";
 import { localeToCountry } from "@/lib/utils/formatters";
+import { LOCALES } from "@/lib/utils/locales";
 
 export function ChangeLang({
     locale
@@ -15,29 +16,78 @@ export function ChangeLang({
     const [currentLocale, setCurrentLocale] = useState<Locale>(locale);
     
     return (
-        <button onClick={() => setIsSelecting(isSelecting => !isSelecting)} className="flex items-center gap-3 border-border border-2 text-text px-3 py-1 rounded-md hover:border-border-hover transition-colors duration-200">
-            <div className="flex items-center gap-2">
-                <ReactCountryFlag
-                    countryCode={localeToCountry(currentLocale)}
-                    svg
-                    style={{
-                        width: "18px",
-                        height: "18px",
-                        borderRadius: "50%",
-                        objectFit: "cover",
-                    }}
-                />
-                <span>{currentLocale.toUpperCase()}</span>
-            </div>
-            <ChevronDown className={`
+        <div className="relative">
+            <button
+                onClick={() => setIsSelecting(isSelecting => !isSelecting)}
+                className="flex items-center gap-3 border-border border-2 text-text px-3 py-1 rounded-md hover:border-border-hover transition-colors duration-200"
+            >
+                <div className="flex items-center gap-2">
+                    <ReactCountryFlag
+                        countryCode={localeToCountry(currentLocale)}
+                        svg
+                        style={{
+                            width: "18px",
+                            height: "18px",
+                            borderRadius: "50%",
+                            objectFit: "cover",
+                        }}
+                    />
+                    <span className="text-text">
+                        {currentLocale.toUpperCase()}
+                    </span>
+                </div>
+                <ChevronDown
+                    className={`
+                        ${isSelecting
+                            ?
+                            "rotate-180"
+                            :
+                            "rotate-0"
+                        }
+                        size-4 stroke-3 transition-transform duration-300
+                    `} />
+            </button>
+
+            <div
+                className={`
                     ${isSelecting
                         ?
-                        "rotate-180"
+                        "opacity-100 scale-100"
                         :
-                        "rotate-0"
+                        "opacity-0 scale-35"
                     }
-                    size-4 stroke-3 transition-transform duration-200
-                `} />
-        </button>
+                    absolute origin-top flex flex-col gap-1 transition-all duration-300 top-full left-1/2 -translate-x-1/2 w-max mt-2 border-border border-2 px-3 py-2 rounded-md
+                    `}
+            >
+                {
+                    LOCALES.filter(locale => locale !== currentLocale).map(locale => 
+                        <button
+                            key={locale}
+                            onClick={
+                                () => {
+                                    setCurrentLocale(locale);
+                                    setIsSelecting(false);
+                                }
+                            }
+                            className="flex gap-2 items-center"
+                        >
+                            <ReactCountryFlag
+                                countryCode={localeToCountry(locale)}
+                                svg
+                                style={{
+                                    width: "18px",
+                                    height: "18px",
+                                    borderRadius: "50%",
+                                    objectFit: "cover",
+                                }}
+                            />
+                            <span className="text-text-muted hover:text-text transition-colors duration-200 text-lg">
+                                {locale.toUpperCase()}
+                            </span>
+                        </button>
+                    )
+                }
+            </div>
+        </div>
     );
 };
