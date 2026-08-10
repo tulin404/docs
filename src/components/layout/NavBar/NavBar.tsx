2e044c9d20193afd323b3cbdadd8055041b127fa
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-10 12:02:48 -0300

    Added border animation

--- src/components/layout/NavBar/NavBar.tsx
+++ src/components/layout/NavBar/NavBar.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { Locale } from "@/types/props";
 import Link from "next/link";
 import { Logo } from "@/components/layout/Logo";
+import { useState, useEffect } from "react";
 
 export function NavBar({
     locale,
@@ -9,8 +12,31 @@ export function NavBar({
     locale: Locale,
     content: any // eslint-disable-line
 }) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        function handleScroll() {
+            setVisible(window.scrollY > 100);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="bg-background border-b-2 border-border px-12 py-4">
+        <nav
+            className={`
+                ${isVisible
+                    ?
+                    "border-border"
+                    :
+                    "border-transparent"
+                }
+                bg-background border-b-2 px-12 py-4 transition-colors duration-200
+            `}>
             {/* NAV LEFT */}
             <div className="flex items-center gap-8">
                 <Logo locale={locale} />
