265f5804073816bb995fe5a5fcaaf26107b5f9df
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-10 22:12:10 -0300

    Styles updated

--- src/components/layout/NavBar/NavBar.tsx
+++ src/components/layout/NavBar/NavBar.tsx
@@ -43,7 +43,7 @@ export function NavBar({
                 <Link
                     href={`/${locale}/docs`}
                     className={`
-                        relative text-text font-primary font-medium
+                        relative text-text font-medium
                         after:absolute after:-inset-2 after:-z-10 after:rounded-full after:bg-text/20 after:blur-lg after:opacity-0 hover:after:opacity-100 after:transition-opacity after:duration-400 after:delay-100
                         `}
                 >
