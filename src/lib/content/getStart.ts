0b62d2ec130d469f99623cdcc08fc20f206498f4
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-12 17:03:12 -0300

    Added spanish translation to start page

--- src/lib/content/getStart.ts
+++ src/lib/content/getStart.ts
@@ -6,15 +6,20 @@ export function getStart(locale: Locale): StartContent {
         case "pt":
             return ({
                 keywords: ["Sistemas", "APIs", "Experimentos", "Soluções", "Implementações"],
-                hero: " que construi, documentado.",
+                hero: "que construi, documentado.",
                 desc: "Projetos, módulos, protótipos e mais."
             });
         case "en":
             return ({
                 keywords: ["Systems", "APIs", "Experiments", "Solutions", "Implementations"],
-                hero: " that I've built, documented.",
+                hero: "that I've built, documented.",
                 desc: "Projects, modules, prototypes and more."
             });
         case "es":
+            return ({
+                keywords: ["Sistemas", "APIs", "Experimentos", "Soluciones", "Implementaciones"],
+                hero: "que construí, documentado.",
+                desc: "Proyectos, módulos, prototipos y más."
+            })
     };
 };
