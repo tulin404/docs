9a57a0bef44fd33bdc1b8c384153ab19ce6be50a
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-13 20:38:03 -0300

    Get card content and apply

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -1,21 +1,34 @@
+import { getCards } from "@/lib/content/getCards";
 import { DocType } from "@/types/docs";
+import { Locale } from "@/types/props";
 import { Layers, Braces, Box, FlaskConical } from "lucide-react";
 
 export function Card({
-    type
+    type,
+    locale
 }: {
-    type: DocType
+    type: DocType,
+    locale: Locale
 }) {
-    const icon = () => {
+    const content = getCards(type, locale)
+    
+    function getIcon() {
         switch(type) {
             case "project":
-                return <Layers />
+                return <Layers className="text-text" />
             case "api":
-                return <Braces />
+                return <Braces className="text-text" />
             case "module":
-                return <Box />
+                return <Box className="text-text" />
             case "experiment":
-                return <FlaskConical />
+                return <FlaskConical className="text-text" />
         };
     };
+
+    return (
+        <div className="p-4 border-2 border-border rounded-lg">
+            {getIcon()}
+            <h2>{}</h2>
+        </div>
+    );
 };
