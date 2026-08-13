2bd7176262f3a6da5c13b46e3bbe783689b5e9a2
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-13 20:39:49 -0300

    Added length for content

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/home/Card";
 import { WordSlider } from "@/components/home/WordSlider";
 import { getStart } from "@/lib/content/getStart";
 import { Params } from "@/types/props";
@@ -12,15 +13,17 @@ export default async function Page({
     const content = getStart(locale);
 
     return (
-        <section className="h-dvh w-dvw flex flex-col justify-center px-36">
-            <div className="flex flex-col gap-4">
+        <main className="h-dvh w-dvw flex flex-col justify-center px-36">
+            <div className="flex flex-col gap-4 pb-8">
                 <h1 className="text-text">
                     <WordSlider content={content} />
                     <span className="text-2xl">{content.hero}</span>
                 </h1>
                 <span className="text-text-muted">{content.desc}</span>
             </div>
-            
-        </section>
+            <div className="flex">
+                <Card type="project" locale={locale} />
+            </div>
+        </main>
     )
 };
--- src/lib/content/getCards.ts
+++ src/lib/content/getCards.ts
@@ -1,5 +1,6 @@
 import { CardContent, DocType } from "@/types/docs";
 import { Locale } from "@/types/props";
+import { PROJECTS, APIS, MODULES, EXPERIMENTS } from "@/constants";
 
 export function getCards(type: DocType, locale: Locale): CardContent {
     function getAllLangs() {
@@ -8,60 +9,72 @@ export function getCards(type: DocType, locale: Locale): CardContent {
                 return ({
                     pt: {
                         title: "Projetos",
-                        desc: "Sistemas e aplicações completas que construí."
+                        desc: "Sistemas e aplicações completas que construí.",
+                        count: PROJECTS.length
                     },
                     en: {
                         title: "Projects",
-                        desc: "Full applications and systems that I've built."
+                        desc: "Full applications and systems that I've built.",
+                        count: PROJECTS.length
                     },
                     es: {
                         title: "",
-                        desc: ""
+                        desc: "",
+                        count: PROJECTS.length
                     }
                 });
             case "api":
                 return ({
                     pt: {
                         title: "Projetos",
-                        desc: "Sistemas e aplicações completas que construí."
+                        desc: "Sistemas e aplicações completas que construí.",
+                        count: APIS.length
                     },
                     en: {
                         title: "Projects",
-                        desc: "Full applications and systems that I've built."
+                        desc: "Full applications and systems that I've built.",
+                        count: APIS.length
                     },
                     es: {
                         title: "",
-                        desc: ""
+                        desc: "",
+                        count: APIS.length
                     }
                 });
             case "module":
                 return ({
                     pt: {
                         title: "Projetos",
-                        desc: "Sistemas e aplicações completas que construí."
+                        desc: "Sistemas e aplicações completas que construí.",
+                        count: MODULES.length
                     },
                     en: {
                         title: "Projects",
-                        desc: "Full applications and systems that I've built."
+                        desc: "Full applications and systems that I've built.",
+                        count: MODULES.length
                     },
                     es: {
                         title: "",
-                        desc: ""
+                        desc: "",
+                        count: MODULES.length
                     }
                 });
             case "experiment":
                 return ({
                     pt: {
                         title: "Projetos",
-                        desc: "Sistemas e aplicações completas que construí."
+                        desc: "Sistemas e aplicações completas que construí.",
+                        count: MODULES.length
                     },
                     en: {
                         title: "Projects",
-                        desc: "Full applications and systems that I've built."
+                        desc: "Full applications and systems that I've built.",
+                        count: MODULES.length
                     },
                     es: {
                         title: "",
-                        desc: ""
+                        desc: "",
+                        count: MODULES.length
                     }
                 });
         };
