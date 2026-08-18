dfaa667e82f73b2cf85aead50a6eb7e8b5a7f2b7
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-17 22:01:41 -0300

    Styled card

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -1,7 +1,7 @@
 import { getCards } from "@/lib/content/getCards";
 import { DocType } from "@/types/docs";
 import { Locale } from "@/types/props";
-import { Layers, Braces, Box, FlaskConical } from "lucide-react";
+import { Layers, Braces, Box, FlaskConical, MoveRight } from "lucide-react";
 import Link from "next/link";
 
 export function Card({
@@ -16,31 +16,33 @@ export function Card({
     function getIcon() {
         switch(type) {
             case "project":
-                return <Layers size={36} className="text-text" />
+                return <Layers size={32} className="text-text" />
             case "api":
-                return <Braces size={36} className="text-text" />
+                return <Braces size={32} className="text-text" />
             case "module":
-                return <Box size={36} className="text-text" />
+                return <Box size={32} className="text-text" />
             case "experiment":
-                return <FlaskConical size={36} className="text-text" />
+                return <FlaskConical size={32} className="text-text" />
         };
     };
 
     return (
         <Link
             href=""
-            className="group p-5 border-2 border-border rounded-lg bg-linear-to-t from-background to-text-disabled/10 hover:border-border-hover transition-colors duration-200 hover:cursor-pointer"
+            className="group flex flex-col gap-6 p-5 border-2 border-border rounded-lg bg-linear-to-t from-background to-text-disabled/10 hover:border-border-hover transition-colors duration-200 hover:cursor-pointer"
         >
             <div className="flex flex-col gap-4">
                 <div className="flex items-center justify-between">
                     {getIcon()}
                     <span className="rounded-md text-text border-2 border-border px-2 py-0.5 group-hover:border-border-hover transition-colors duration-200">{content.count}</span>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-1">
                     <h2 className="text-text text-2xl tracking-wide">{content.title}</h2>
                     <span className="text-text-muted">{content.desc}</span>
                 </div>
             </div>
+            <div className="group-hover:bg-border-hover bg-border h-0.5 transition-colors duration-200" />
+            <MoveRight className="group-hover:translate-x-4 transition-transform duration-400 text-text" />
         </Link>
     );
 };
