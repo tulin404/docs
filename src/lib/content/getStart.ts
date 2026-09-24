1d6d50165e256b33f92ee38624135f74d59ab2d0
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-09-24 17:00:59 -0300

    Added recent to startContent

--- src/lib/content/getStart.ts
+++ src/lib/content/getStart.ts
@@ -7,19 +7,22 @@ export function getStart(locale: Locale): StartContent {
             return ({
                 keywords: ["Sistemas", "APIs", "Experimentos", "Soluções", "Implementações"],
                 hero: "que construí, documentado.",
-                desc: "Projetos, módulos, protótipos e mais."
+                desc: "Projetos, módulos, protótipos e mais.",
+                recent: "Atualizados recentemente"
             });
         case "en":
             return ({
                 keywords: ["Systems", "APIs", "Experiments", "Solutions", "Implementations"],
                 hero: "that I've built, documented.",
-                desc: "Projects, modules, prototypes and more."
+                desc: "Projects, modules, prototypes and more.",
+                recent: "Recently updated"
             });
         case "es":
             return ({
                 keywords: ["Sistemas", "APIs", "Experimentos", "Soluciones", "Implementaciones"],
                 hero: "que construí, documentado.",
-                desc: "Proyectos, módulos, prototipos y más."
+                desc: "Proyectos, módulos, prototipos y más.",
+                recent: "Actualizados recientemente"
             })
     };
 };
